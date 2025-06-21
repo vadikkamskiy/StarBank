c7ade4e87c29afebd295a6f7a3e1e6f5e66eb839
vadikkamskiy <vadikkamskiy@gmail.com>
2025-06-21 20:55:14 +0300

    first commit

---
+++ src/main/java/com/skypro/StarBank/StarBankApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.StarBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/StarBank/StarBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.StarBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
