c11000fe9e4d97484d232fbfbc8618549fc1261a
vadikkamskiy <vadikkamskiy@gmail.com>
2025-07-05 23:21:46 +0300

    Remove ResponseEntity

--- src/main/java/com/skypro/StarBank/controller/DynamicRuleController.java
+++ src/main/java/com/skypro/StarBank/controller/DynamicRuleController.java
@@ -5,6 +5,8 @@ import com.skypro.StarBank.dto.response.DynamicRuleResponse;
 import com.skypro.StarBank.model.DynamicRule;
 import com.skypro.StarBank.model.Query;
 import com.skypro.StarBank.repository.DynamicRuleRepository;
+import com.skypro.StarBank.service.DynamicRuleService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,44 +19,30 @@ import java.util.stream.Collectors;
 @RequestMapping("/rule")
 public class DynamicRuleController {
     private final DynamicRuleRepository ruleRepository;
+    private final DynamicRuleService ruleService;
 
-    public DynamicRuleController(DynamicRuleRepository ruleRepository) {
+    public DynamicRuleController(DynamicRuleRepository ruleRepository, DynamicRuleService ruleService) {
         this.ruleRepository = ruleRepository;
+        this.ruleService = ruleService;
     }
 
     @PostMapping
-    public ResponseEntity<DynamicRuleResponse> createRule(@RequestBody DynamicRuleRequest request) {
-        DynamicRule rule = new DynamicRule();
-        rule.setName(request.getName());
-        rule.setText(request.getText());
-        rule.setRule(request.getRule().stream()
-                .map(q -> {
-                    Query query = new Query();
-                    query.setQuery(q.getQuery());
-                    query.setArguments(q.getArguments());
-                    query.setNegate(q.isNegate());
-                    return query;
-                }).collect(Collectors.toList()));
-        ruleRepository.save(rule);
-        return ResponseEntity.ok(new DynamicRuleResponse(
-                rule.getName(),
-                rule.getId(),
-                rule.getText(),
-                request.getRule()
-        ));
+    public DynamicRuleResponse createRule(@RequestBody DynamicRuleRequest request) {
+        return ruleService.createRule(request);
     }
+
 
 
     @GetMapping
-    public ResponseEntity<Map<String, List<DynamicRule>>> getAllRules() {
+    public Map<String, List<DynamicRule>> getAllRules() {
         Map<String, List<DynamicRule>> response = new HashMap<>();
         response.put("data", ruleRepository.findAll());
-        return ResponseEntity.ok(response);
+        return response;
     }
 
     @DeleteMapping("/{product_id}")
-    public ResponseEntity<Void> deleteRule(@PathVariable String product_id) {
+    public void deleteRule(@PathVariable String product_id) {
         ruleRepository.deleteById(product_id);
-        return ResponseEntity.noContent().build();
+
     }
 }
--- src/main/java/com/skypro/StarBank/repository/DynamicRuleRepository.java
+++ src/main/java/com/skypro/StarBank/repository/DynamicRuleRepository.java
@@ -1,8 +1,14 @@
 package com.skypro.StarBank.repository;
 
 import com.skypro.StarBank.model.DynamicRule;
+
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface DynamicRuleRepository extends JpaRepository<DynamicRule, String> {}
+@Repository
+public interface DynamicRuleRepository extends JpaRepository<DynamicRule, String> {
+    public void deleteById(String id);
+}
